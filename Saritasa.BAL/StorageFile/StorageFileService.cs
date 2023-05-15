c5b891e2daf4682bae21d5fad97ed10db67755b2
Linh Mac Le My <linlin.18dh110774huflit@gmail.com>
2023-05-16 00:59:40 +0700

    Fix code, Init unit test

--- Saritasa.BAL/StorageFile/StorageFileService.cs
+++ Saritasa.BAL/StorageFile/StorageFileService.cs
@@ -21,11 +21,11 @@ namespace Saritasa.BAL.StorageFile
         {
             _dbContext = dbContext;
         }
-
+        // Text funtions
         public async Task<string> AccessTextAsync(string id, string updatedUser)
         {
             // Check text exist
-            var file = await _dbContext.StorageTexts!.FirstOrDefaultAsync(x => x.ID.ToString() == id);
+            var file = await _dbContext.StorageTexts!.Where(x => x.ID.ToString() == id).FirstOrDefaultAsync();
             // Text not exist, return
             if (file == null) return "";
             // If DownloadOnce flag was turned on --> Delete, else update the latest user access it
@@ -41,98 +41,6 @@ namespace Saritasa.BAL.StorageFile
             
             return file.Content;
         }
-
-        public async Task<bool> CreateFileAsync(Guid id, Guid userID, string path, bool downloadOnce)
-        {
-            var file = new Data.Entities.StorageFile()
-            {
-                ID = id,
-                DownloadOnce = downloadOnce,
-                Url = path,
-                UserID = userID,
-                UpdateByUserID = userID.ToString(),
-                FileState = FileStateEnum.InUse
-            };
-            await _dbContext.StorageFiles!.AddAsync(file);
-            await _dbContext.SaveChangesAsync();
-            return true;
-        }
-
-        public async Task<string> CreateTextAsync(CreateTextRequest input, Guid userID)
-        {
-            var id = Guid.NewGuid();
-            var file = new Data.Entities.StorageText()
-            {
-                ID = id,
-                DownloadOnce = input.DownloadOnce,
-                Content = input.Text,
-                UserID = userID,
-                UpdateByUserID = userID.ToString(),
-                FileState = FileStateEnum.InUse
-            };
-            await _dbContext.StorageTexts!.AddAsync(file);
-            await _dbContext.SaveChangesAsync();
-            return id.ToString();
-        }
-
-        public async Task<bool> DeleteFileAsync(string id)
-        {
-            var file = await _dbContext.StorageFiles!.FirstOrDefaultAsync(x => x.ID.ToString() == id);
-            if (file == null) return false;
-            _dbContext.StorageFiles!.Remove(file);
-            await _dbContext.SaveChangesAsync();
-            return true;
-        }
-
-        public async Task<bool> DeleteTextAsync(string id)
-        {
-            var file = await _dbContext.StorageTexts!.FirstOrDefaultAsync(x => x.ID.ToString() == id);
-            if(file == null) return false;
-            _dbContext.StorageTexts!.Remove(file);
-            await _dbContext.SaveChangesAsync();
-            return true;
-        }
-
-        public async Task<List<FileViewModel>> GetFilesAsync()
-        {
-            // Query list of texts
-            var query = from t in _dbContext.StorageFiles
-                        where t.FileState == FileStateEnum.InUse
-                        select t;
-            // Convert from db entity to View Model
-            var data = await query.Select(
-               t => new FileViewModel()
-               {
-                   ID = t.ID,
-                   Path = t.Url,
-                   DownloadOnce = t.DownloadOnce
-               }).ToListAsync();
-
-            return data;
-        }
-
-        public async Task<string> GetServerFileURLAsync(string id, string updatedUser)
-        {
-            // Check file exist
-            var file = await _dbContext.StorageFiles!.FirstOrDefaultAsync(x => x.ID.ToString() == id);
-            // File not exist, return
-            if (file == null) return "";
-            // If DownloadOnce flag was turned on --> Delete, else update the latest user access it
-            if (file.DownloadOnce)
-            {
-                _dbContext.StorageFiles!.Remove(file);
-
-                System.IO.File.Delete(file.Url);
-            }
-            else
-            {
-                file.UpdateByUserID = updatedUser;
-            }
-            await _dbContext.SaveChangesAsync();
-
-            return file.Url;
-        }
-
         public async Task<List<TextViewModel>> GetTextsAsync()
         {
             // Query list of texts
@@ -150,5 +58,93 @@ namespace Saritasa.BAL.StorageFile
 
             return data;
         }
+        public async Task<string> CreateTextAsync(CreateTextRequest input, Guid userID)
+        {
+            var id = Guid.NewGuid();
+            var file = new Data.Entities.StorageText()
+            {
+                ID = id,
+                DownloadOnce = input.DownloadOnce,
+                Content = input.Text,
+                UserID = userID,
+                UpdateByUserID = userID.ToString(),
+                FileState = FileStateEnum.InUse
+            };
+            await _dbContext.StorageTexts!.AddAsync(file);
+            await _dbContext.SaveChangesAsync();
+            return id.ToString();
+        }
+        public async Task<bool> DeleteTextAsync(string id)
+        {
+            var file = await _dbContext.StorageTexts!.Where(x => x.ID.ToString() == id).FirstOrDefaultAsync();
+            if (file == null) return false;
+            _dbContext.StorageTexts!.Remove(file);
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+
+        // File functions
+        public async Task<string> GetServerFileURLAsync(string id, string updatedUser)
+        {
+            // Check file exist
+            var file = await _dbContext.StorageFiles!.Where(x => x.ID.ToString() == id).FirstOrDefaultAsync();
+            // File not exist, return
+            if (file == null) return "";
+            // If DownloadOnce flag was turned on --> Delete, else update the latest user access it
+            if (file.DownloadOnce)
+            {
+                _dbContext.StorageFiles!.Remove(file);
+
+                System.IO.File.Delete(file.Url);
+            }
+            else
+            {
+                file.UpdateByUserID = updatedUser;
+            }
+            await _dbContext.SaveChangesAsync();
+
+            return file.Url;
+        }
+        public async Task<List<FileViewModel>> GetFilesAsync()
+        {
+            // Query list of texts
+            var query = from t in _dbContext.StorageFiles
+                        where t.FileState == FileStateEnum.InUse
+                        select t;
+            // Convert from db entity to View Model
+            var data = await query.Select(
+               t => new FileViewModel()
+               {
+                   ID = t.ID,
+                   Path = t.Url,
+                   DownloadOnce = t.DownloadOnce
+               }).ToListAsync();
+
+            return data;
+        }
+        public async Task<bool> CreateFileAsync(Guid id, Guid userID, string path, bool downloadOnce)
+        {
+            var file = new Data.Entities.StorageFile()
+            {
+                ID = id,
+                DownloadOnce = downloadOnce,
+                Url = path,
+                UserID = userID,
+                UpdateByUserID = userID.ToString(),
+                FileState = FileStateEnum.InUse
+            };
+            await _dbContext.StorageFiles!.AddAsync(file);
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+        public async Task<bool> DeleteFileAsync(string id)
+        {
+            var file = await _dbContext.StorageFiles!.Where(x => x.ID.ToString() == id).FirstOrDefaultAsync();
+            if (file == null) return false;
+            _dbContext.StorageFiles!.Remove(file);
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+        
     }
 }
---
+++ Saritasa.Unittest/ServiceTest/StorageFileServiceTest.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Saritasa.BAL.StorageFile;
+using Saritasa.Data.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saritasa.Unittest.ServiceTest
+{
+    public class StorageFileServiceTest
+    {
+        [Fact]
+        public async Task AccessTextAsync_NotFoundText()
+        {
+            var builder = new DbContextOptionsBuilder<LocalDBContext>().UseInMemoryDatabase(Guid.NewGuid().ToString());
+
+            var dbcontext = new LocalDBContext(builder.Options);
+
+            var storageFileService = new StorageFileService(dbcontext);
+
+            var result = await storageFileService.AccessTextAsync("0A586AC2-DCD4-4E53-6211-08DB53C440D3", "Guest");
+
+            Assert.Equal("", result);
+        }
+
+        [Fact]
+        public async Task AccessTextAsync_ReadText()
+        {
+            // Arrange
+            var builder = new DbContextOptionsBuilder<LocalDBContext>().UseInMemoryDatabase(Guid.NewGuid().ToString());
+
+            var dbcontext = new LocalDBContext(builder.Options);
+            dbcontext.StorageTexts.Add(new Data.Entities.StorageText()
+            {
+                ID = new Guid("0A586AC2-DCD4-4E53-6211-08DB53C440D3"),
+                Content = "Test",
+                DownloadOnce = false
+            });
+            await dbcontext.SaveChangesAsync();
+
+            var storageFileService = new StorageFileService(dbcontext);
+
+            // Act
+            // Check read text successfully
+            var result = await storageFileService.AccessTextAsync("0A586AC2-DCD4-4E53-6211-08DB53C440D3", "Guest");
+            // Check text was deleted
+            //var result2 = await storageFileService.AccessTextAsync("0A586AC2-DCD4-4E53-6211-08DB53C440D3", "Guest");
+
+            // Assert
+            Assert.Equal("Test", result);
+            //Assert.Equal("", result2);
+        }
+    }
+}
